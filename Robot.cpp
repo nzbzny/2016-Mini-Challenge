36582438f5a227d305e97eea750f5dfe2e9ca868
Noah Z <nzbzny@users.noreply.github.com>
2016-12-06 15:40:46 -0500

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -3,6 +3,8 @@
 Robot::Robot() :
 	position(),
 	robotDrive(Constants::driveLeftPin, Constants::driveRightPin, position),
+	arm(),
+	track(),
 	driveStick(Constants::driveStickChannel),
 	frontUltrasonic(Constants::frontUltrasonicPin),
 	backUltrasonic(Constants::backUltrasonicPin),
@@ -16,22 +18,46 @@ void Robot::OperatorControl() //teleop code
 	robotDrive.SetSafetyEnabled(false);
 	compressor.Start();
 
-	//track.releaseSmallBalls();
+	track.openBottom();
 
 	while(IsOperatorControl() && IsEnabled())
 	{
 		float leftValue = throttle * driveStick.GetRawAxis(Constants::PS4LeftJoystick);
 		float rightValue = throttle * driveStick.GetRawAxis(Constants::PS4RightJoystick);
-		robotDrive.TankDrive(leftValue, rightValue, true);
+		robotDrive.TankDriveSpeed(leftValue, rightValue);
+		if (driveStick.getRawButton(Constants::raiseArmButton))
+			arm.raise();
+		if (driveStick.getRawButton(Constants::lowerArmButton))
+			arm.lower();
+		if (driveStick.getRawButton(Constants::grabContainerButton))
+			arm.close();
+		if (driveStick.getRawButton(Constants::releaseContainerButton))
+			arm.open();
+		if (driveStick.getRawButton(Constants::releaseBigBallsButton))
+			track.openTop();
+		if (driveStick.getRawButton(Constants::releaseSmallBallsButton))
+			track.openBottom();
+		if (driveStick.getRawButton(Constants::releaseAllBallsButton))
+			{
+				track.openTop();
+				track.openBottom();
+			}
+		if (driveStick.getRawButton(Constants::lockBallsButton))
+			{
+				track.closeTop();
+				track.closeBottom();
+			}
 	}
 
 	compressor.Stop();
+	robotDrive.SetSafetyEnabled(true);
 }
 
 
 
 void Robot::Autonomous() {
 	//use navx mxp
+	robotDrive.SetSafetyEnabled(false);
 	compressor.Start();
 
 	//track.lock();
@@ -47,13 +73,15 @@ void Robot::Autonomous() {
 	}
 	robotDrive.TankDriveSpeed(0,0);
 
-	//arm.down();
+	arm.lower();
+	
+	arm.stop();
 
-	//gripper.grab();
+	gripper.close();
 
-	//arm.up(); (separate thread probably)
-
-	Wait(1);//Temporary so robot doesn't flip
+	arm.raise();
+	
+	arm.stop();
 
 	fs = 0;
 	while (frontUltrasonic.getVoltage() * Constants::ultrasonicVoltageToInches > Constants::minDistanceToCheeseballs && fs < 500)
@@ -66,9 +94,10 @@ void Robot::Autonomous() {
 	}
 	robotDrive.TankDriveSpeed(0,0);
 
-	//track.releaseBigBalls();
+	track.openTop();
 
 	compressor.Stop();
+	robotDrive.SetSafetyEnabled(true);
 }
 
 START_ROBOT_CLASS(Robot);
